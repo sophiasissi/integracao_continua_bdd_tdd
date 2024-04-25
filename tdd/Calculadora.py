07e181e9de980d763b2fc7945d76e1cce4329298
Sophia Sissi <sophiacurcioguedes@gmail.com>
2024-04-25 11:49:42 -0300

    Implementando testes

---
+++ tdd/Calculadora.py
@@ -0,0 +1,5 @@
+class Calculadora: 
+    
+    @staticmethod
+    def media(a, b):
+        return (a + b) / 2
---
+++ tdd/test_media.py
@@ -0,0 +1,3 @@
+def test_media_num(self): 
+    result = Calculadora.media(2, 3)
+    assert result == 2.5
